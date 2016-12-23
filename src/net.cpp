e1a0e2f228e779a5443a2d16c30bc06dec225a7d
economicalcoin <econ@mailinator.com>
2016-12-23 17:35:15 +0700

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1229,6 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+    {"212.24.110.56", "212.24.110.56"},
     {"95.211.57.108", "95.211.57.108"},
 };
 
